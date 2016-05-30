9eb0fb72ec598ff1e2db78f69d93f67dbcfe783f
Helmut Drewes <helmut.drewes@agrista.com>
2016-05-30 14:46:23 +0200

    [IMP] Added material design blog layout

--- theme_material/static/src/js/navbar.js
+++ theme_material/static/src/js/navbar.js
@@ -1,11 +1,15 @@
 $(document).ready(function () {
 
-    var navbar = $('header .navbar'),
-        offset = $('#oe_main_menu_navbar').height();
+    var navbar = $('#md-navbar'),
+        navbarHeight = navbar.height(),
+        offset = $('#oe_main_menu_navbar').outerHeight(),
+        wrapwrap = $('#wrapwrap');
 
-    console.log(offset);
+    //console.log(offset);
+    //console.log(navbarHeight);
 
-    //navbar.css('margin-top', offset);
-    navbar.css('margin-top', 0);
-
+    wrapwrap.css('padding-top', offset+navbarHeight);
+    navbar.css('margin-top',offset);
+    navbar.removeClass('navbar-static-top');
+    navbar.addClass('navbar-fixed-top');
 });
