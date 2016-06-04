136634079b9d45e09c21d41b281af56c1a9cc6d0
Helmut Drewes <helmut.drewes@agrista.com>
2016-06-04 10:28:26 +0200

    [IMP] Added multiple brands

---
+++ website_abr/__openerp__.py
@@ -0,0 +1,12 @@
+# noinspection PyStatementEffect
+{
+    'name': 'ABR Africa Logo',
+    'summary': 'ABR Africa logo on website',
+    'description': 'Modification to display custom logo.',
+    'category': 'Website',
+    'version': '1.0',
+    'depends': ['theme_material', ],
+    'data': ['views/templates.xml',
+             ],
+    'images': ['static/description/material.png'],
+}
